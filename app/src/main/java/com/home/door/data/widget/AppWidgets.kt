8ea98dcda686a08783b7af842a31680c4fabb85f
taha-mohammed <tahass3512an@gmail.com>
2023-08-21 07:00:10 +0300

    migrating from kapt to ksp annotation processor

--- app/src/main/java/com/home/door/data/widget/AppWidgets.kt
+++ app/src/main/java/com/home/door/data/widget/AppWidgets.kt
@@ -1,10 +1,7 @@
 package com.home.door.data.widget
 
-import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.PersistentList
-import kotlinx.collections.immutable.persistentListOf
+
 import kotlinx.serialization.Serializable
-import okhttp3.internal.immutableListOf
 
 @Serializable
 data class AppWidgets(
